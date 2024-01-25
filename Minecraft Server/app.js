c6f3e37c53b3e26ba81c6806b383c6906c693091
danzellil <146328091+danzellil@users.noreply.github.com>
2024-01-25 08:16:11 +0300

    Add files via upload

---
+++ Minecraft Server/app.js
@@ -0,0 +1,15 @@
+
+function toggle_visibility() {
+    var e = document.getElementById("donate");
+    if(e.style.display == 'block')
+       e.style.display = 'none';
+    else
+       e.style.display = 'block';
+}
+function toggle_visibility2() {
+    var e = document.getElementById("role");
+    if(e.style.display == 'block')
+       e.style.display = 'none';
+    else
+       e.style.display = 'block';
+}
